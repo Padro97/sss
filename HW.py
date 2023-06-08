380a099a4773fb31b9dd0737e368dfb3714ae229
Nikita Pyshkov <85805562+pyshkovni@users.noreply.github.com>
2023-06-08 10:33:51 +0300

    Доделал комментарии к коду

--- HW.py
+++ HW.py
@@ -10,6 +10,10 @@ import re
 excel_data_df = pd.read_excel('phone_numbers.xlsx')
 print(excel_data_df)
 
+
+# Затем, создаётся функция 'clean_phone' для сортировки номеров от ненужных символов
+# Если исходное значение не соответствует критериям телефонного номера, 
+# то рядом с этим номером будет выведено 'не соответствует телефонному номеру'
 def clean_phone(phone):
     cleaned_phone = re.sub(r'\D', '', str(phone))
     if cleaned_phone.startswith('8'):
@@ -18,6 +22,11 @@ def clean_phone(phone):
         cleaned_phone = cleaned_phone +', не соответствует телефонному номеру'
     return cleaned_phone
 
+# Далее, первой же командной программа снова считывает содержимое файла 'phone_numbers.xlsx' 
+# Вторая команда считывает значения из столбца 'phone_number' и применяет к ним функци. 'clean_phone' 
+# Заключительная команда переносит очищенные номера телефонов в новую таблицу 'new.xlsx.
+
 df = pd.read_excel('phone_numbers.xlsx')
 df['phone_number'] = df['phone_number'].apply(clean_phone)
 df.to_excel('new.xlsx', index=False)
+
