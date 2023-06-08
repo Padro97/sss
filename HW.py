cee13724e2a32a95f628b78fbc6512447bc253e3
Nikita Pyshkov <85805562+pyshkovni@users.noreply.github.com>
2023-06-08 10:28:23 +0300

    Добавил первые комментарии
    Переношу из одного из ридми текст в качестве комментов

--- HW.py
+++ HW.py
@@ -1,8 +1,15 @@
+##### Очистка номеров телефонов #####
+
+# Первые две строки импортируют две библиотеки: pandas и re. 
+
 import pandas as pd
 import re
 
+# Далее, с помощью команды excel_data_df = pd.read_excel('phone_numbers.xlsx') открывается и считывается таблица 'phone_nembers.xlsx', 
+# и следующей же командой выводится содержимре этого файла
 excel_data_df = pd.read_excel('phone_numbers.xlsx')
 print(excel_data_df)
+
 def clean_phone(phone):
     cleaned_phone = re.sub(r'\D', '', str(phone))
     if cleaned_phone.startswith('8'):
@@ -10,6 +17,7 @@ def clean_phone(phone):
     elif not cleaned_phone.startswith(('7', '8')):
         cleaned_phone = cleaned_phone +', не соответствует телефонному номеру'
     return cleaned_phone
+
 df = pd.read_excel('phone_numbers.xlsx')
 df['phone_number'] = df['phone_number'].apply(clean_phone)
 df.to_excel('new.xlsx', index=False)
